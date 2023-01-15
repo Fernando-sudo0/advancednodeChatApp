c3fcd9c0b2ffc8b2b95fb2db762fb08588c94838
Fernando Gutierrez <fernando.gtz.escarrega@gmail.com>
2023-01-14 20:47:57 -0800

    { whatever

--- server.js
+++ server.js
@@ -90,32 +90,32 @@ app.route('/register')
   //1. Register the new user
   //1.1 Query database with findOne
 
-   myDataBase.findOne({username : req.body.username}, function(err, doc){
+   myDataBase.findOne({ username : req.body.username }, function(err, doc){
      //1.2 If there's an error, call next with error
     if(err)
       next(err)
       //1.3 If a user is returned, redirect back to home
     else if(doc ){
-      res.redirect('/')
+      res.redirect('/');
       //1.4 If a user is not found and no error ocurr, then insert One.
       //    authenticating the new user, which you already wrote the logic  
       //    for in your POST /login route.  
     }else{
-      myDataBase.insertOne({username : req.body.username, password : req.body.password}, function(err, doc) {
+      myDataBase.insertOne({ username : req.body.username, password : req.body.password }, function(err, doc) {
         if(err)
-          res.redirect('/')
+          res.redirect('/');
         else
       
-          next(null,doc.ops[0])
+          next(null, doc.ops[0]);
       })
     }      
    })
 }, 
 //    As long as no errors occur there, call next to go to step 2, 
 //2 . Authenticate the new user
-passport.authenticate('local', {failureRedirect: '/'}), (req, res, next)=>{
+passport.authenticate('local', { failureRedirect: '/' }), (req, res, next)=>{
   //Redirect to profile
- res.redirect('/profile')
+ res.redirect('/profile');
 }
 );
 
